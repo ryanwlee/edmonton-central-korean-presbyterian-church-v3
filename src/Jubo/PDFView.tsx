83137683fcde165690d9af6e697a598d659902b0
ryanwoolee <ryanwoolee@gmail.com>
2026-05-14 21:09:24 -0700

    Updates

--- src/Jubo/PDFView.tsx
+++ src/Jubo/PDFView.tsx
@@ -1,7 +1,5 @@
 import { Document, Page, pdfjs } from 'react-pdf';
 import { useState } from 'react';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 // Set worker URL
 pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
